593a9e63f50e66d217d7177cc974ae8c4ea386fd
muradshahin <shahinmurad43@gmail.com>
2025-05-16 22:00:39 +0300

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,14 +5,14 @@ import fs from 'fs';
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-   /**user: process.env.DB_USER,
+    //connectionString: process.env.DATABASE_URL,
+   user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '5432'),
-    database: process.env.DB_NAME, */
+    database: process.env.DB_NAME,
         ssl: {
-            ca: fs.readFileSync('global-bundle.pem').toString()
+                rejectUnauthorized: false
         }
 
     }
